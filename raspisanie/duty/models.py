c4e2c638e9cdbc4d05b0803639271e151b60bf9c
tspyrk <krypst@gmail.com>
2021-01-11 01:57:42 +0500

    Делает html табличку число - день недели

--- raspisanie/duty/models.py
+++ raspisanie/duty/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-
+# from mptt.models import MPTTModel, TreeForeignKey
+#
+# class Genre(MPTTModel):
+#     name = models.CharField(max_length=50, unique=True)
+#     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+#
+#     class MPTTMeta:
+#         order_insertion_by = ['name']
 
 class People(models.Model):
     familia = models.CharField('Фамилия', max_length=10)
--- raspisanie/duty/views.py
+++ raspisanie/duty/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
+import datetime
+import calendar
+import pytils
 
 def index(request):
+
+    c = calendar.Calendar()
+    today = datetime.datetime.now()
+    god, mes = today.year, today.month
+    now = pytils.dt.ru_strftime(u"%d - %a", inflected=True, date=today)
+    # now = pytils.dt.ru_strftime(u"сегодня - %d %B %Y, %A", inflected=True, date=today)
+    table = []
+
+    # print(now, '\n')
+
+    for i in c.itermonthdays(god, mes):
+        if i != 0:
+            day = datetime.date(god, mes, i)
+            n_d = calendar.weekday(god, mes, i)
+            stroka = pytils.dt.ru_strftime(u"%d - %a", inflected=True, date=day)
+            if n_d in range(5):
+                # print(f'{stroka}')
+                table.append(stroka)
+            elif n_d == 5:
+                # print(' ')
+                table.append('')
+            else:                       #
+                table.append(stroka)    # надо будет убрать
+
+
     content = {
         'title': 'Расписание дежурств',
         'txt': 'Расписание дежурств',
+        'now': now,
+        'table': table,
     }
     return render(request, 'duty/index.html', content)

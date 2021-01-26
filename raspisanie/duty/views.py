1eca17bb17d1436c933ed246b1444beea4d8b7f3
Тихоненок Александр Л <atihonenok@grc.ru>
2021-01-26 14:08:45 +0500

    Добавлено предупреждение внизу сайта о "как есть" Улалил пару .DS_Store Замена в расписании Добавлна функция логирования посетителей в текстовый файл

--- raspisanie/duty/views.py
+++ raspisanie/duty/views.py
@@ -4,6 +4,7 @@ import datetime
 import pytils
 from .models import Month
 from .models import People
+from .ips import a
 
 def index(request):
     today = datetime.datetime.now()
@@ -30,4 +31,20 @@ def index(request):
         'now': now,
         'wtf': stroki,
     }
+    def client_ip(reguest):
+        x_forwardwd_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwardwd_for:
+            ip = x_forwardwd_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+        for i in a:
+            if i[2] == ip:
+                posetil = f'{i[1]} - {i[0]} - {i[2]}'
+        print(f'Сайт открыл  {datetime.datetime.now()} - {posetil}')
+        print(type(ip))
+        with open('ip.txt', 'a', encoding='utf-8') as bas:
+            bas.write(f"{datetime.datetime.now()} - {posetil} \n")
+        return
+    client_ip(request)
     return render(request, 'duty/index.html', content)
